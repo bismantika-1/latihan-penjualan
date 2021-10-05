b89b19409aed8ddd6a6d2d0bff0853578387d8e3
bismantika-1 <bismantika05@gmail.com>
2021-10-05 11:11:22 +0800

    PHP MVC Penjualaln

---
+++ application/views/barang/tambah.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <div class="row mt-3">
+        <div class="col-md-6">
+            <div class="form" method="post">
+            <div class="mb-3">
+                <label for="kd_barang" class="form-label">Kode Barang</label>
+                <input type="text" class="form-control" name="kd_barang" id="kd_barang">
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
